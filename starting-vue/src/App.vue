19bf19f8409dee19df74644837b156a675c27911
Santiago Gómez de la Torre Romero <santiago.gomez@evenbytes.com>
2025-10-08 10:28:40 +0200

    first code in vue

---
+++ starting-vue/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+// @ts-ignore: no declaration file for Vue SFC
+import MyComponent from "./components/MyComponent.vue";
+</script>
+
+<template>
+  <MyComponent />
+</template>
---
+++ starting-vue/src/components/MyComponent.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="my-component">
+    <h1 v-if="isVisible">{{ title }}</h1>
+    <h2 v-bind:class="{ highlighted: isHighlighted }">{{ name }}</h2>
+    <p>This is a simple Vue component.</p>
+    <button @click="toggleVisibility">
+      {{ isVisible ? "Hide" : "Show" }} Title
+    </button>
+  </div>
+  <hr />
+  <input type="text" v-model="title" placeholder="Type something..." />
+  <button @click="sayHello">Make alert</button>
+  <hr />
+  <button @click="toggleHighlight">
+    {{ isHighlighted ? "Remove Highlight" : "Highlight Name" }}
+  </button>
+  <hr />
+  <ul>
+    <li v-for="item in list" :key="item.id">{{ item.name }}</li>
+  </ul>
+
+  <div id="app">
+    <h1>{{ storeTitle }}</h1>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      title: "Hello, Vue!",
+      name: "Santiago",
+      isHighlighted: false,
+      isVisible: true,
+      list: [
+        { id: 1, name: "Item 1" },
+        { id: 2, name: "Item 2" },
+        { id: 3, name: "Item 3" },
+      ],
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.isVisible = !this.isVisible;
+    },
+    sayHello() {
+      alert(this.title);
+    },
+    toggleHighlight() {
+      this.isHighlighted = !this.isHighlighted;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.highlighted {
+  color: #48e369;
+  font-weight: bold;
+}
+</style>
